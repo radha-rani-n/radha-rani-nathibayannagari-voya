9b8166f135d5d62284c2f32e5752d286e48bc88a
radha-rani-n <nradharani1998@gmail.com>
2025-02-13 11:54:53 -0500

    reset global styles and add pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.scss";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
