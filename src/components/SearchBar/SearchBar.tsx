2a8d442379b08e0b26e7751526f4415e85198cda
radha-rani-n <nradharani1998@gmail.com>
2025-02-15 13:47:49 -0500

    import search bar in home page

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,8 +3,13 @@ import { Input } from "antd";
 const SearchBar = () => {
   return (
     <>
-      <section>
-        <Input type="text" />
+      <section className="search-bar">
+        <Input.Search
+          type="text"
+          placeholder="Enter place name"
+          size="large"
+          //   enterButton="Search"
+        />
       </section>
     </>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,11 @@
 import "./HomePage.scss";
-
+import SearchBar from "../../components/SearchBar/SearchBar";
 import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <>
       <Link to={"/search-result"}>
-        <h2>Search bar</h2>
+        <SearchBar />
       </Link>
       <Link to="/plan-trip">
         <button>Plan a trip</button>
