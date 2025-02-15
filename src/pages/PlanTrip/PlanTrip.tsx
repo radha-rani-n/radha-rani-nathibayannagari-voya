bba29a436b5e446002a424a1b379aae4ae7f321b
radha-rani-n <nradharani1998@gmail.com>
2025-02-15 17:31:02 -0500

    add state variables and onChange functions for form elements

--- src/pages/PlanTrip/PlanTrip.tsx
+++ src/pages/PlanTrip/PlanTrip.tsx
@@ -1,27 +1,51 @@
 import { Input } from "antd";
-// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-// import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
 import { Button } from "antd";
+import { useState } from "react";
 const PlanTrip = () => {
+  const [tripName, setTripName] = useState(null);
+  const [placeName, setPlaceName] = useState(null);
+  const [fromDate, setFromDate] = useState(null);
+  const [toDate, setToDate] = useState(null);
+  const [travellerCount, setTravellerCount] = useState(null);
+  const API_URL = import.meta.env.VITE_API_URL;
+
   const handleOnFormSubmit = () => {};
+
+  const handleTripName = () => {};
+  const handlePlaceName = () => {};
+
+  const handleFromDate = () => {};
+
+  const handleToDate = () => {};
+  const handleTravellers = () => {};
   return (
     <form onSubmit={handleOnFormSubmit}>
       <label>
         Trip Name:
-        <Input type="text" placeholder="Enter trip name" size="large" />
+        <Input
+          type="text"
+          placeholder="Enter trip name"
+          size="large"
+          onChange={handleTripName}
+        />
       </label>
       <label>
         Place Name:
-        <Input type="text" placeholder="Enter place name" size="large" />
+        <Input
+          type="text"
+          placeholder="Enter place name"
+          size="large"
+          onChange={handlePlaceName}
+        />
       </label>
       <label>
         From Date:
-        <Input type="date" size="large" />
+        <Input type="date" size="large" onChange={handleFromDate} />
       </label>
       <label>
         To Date:
-        <Input type="date" size="large" />
+        <Input type="date" size="large" onChange={handleToDate} />
       </label>
       <label>
         No Of Travellers:
@@ -29,6 +53,7 @@ const PlanTrip = () => {
           type="number"
           placeholder="Enter no. of travellers"
           size="large"
+          onChange={handleTravellers}
         />
       </label>
       <Button>Save Trip</Button>
