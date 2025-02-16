d31e653503e30e286f5974342da69b8db74d1206
radha-rani-n <nradharani1998@gmail.com>
2025-02-15 22:51:30 -0500

    create a function to fetch seaved trips

--- src/pages/YourTrips/YourTrips.tsx
+++ src/pages/YourTrips/YourTrips.tsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react";
+import axios from "axios";
+import "./YourTrips.scss";
+
 const YourTrips = () => {
-  return <h2>Your Trips</h2>;
+  const API_URL = import.meta.env.VITE_API_URL;
+  useEffect(() => {
+    const fetchTrips = async () => {
+      try {
+        const { data } = await axios.get(`API_URL`);
+      } catch (e) {
+        console.error(`Error getting trips : ${e}`);
+      }
+    };
+    fetchTrips();
+  });
+  return (
+    <section>
+      <h2>Your planned trips</h2>
+    </section>
+  );
 };
 export default YourTrips;
