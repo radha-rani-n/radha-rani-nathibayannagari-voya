d4d05ee86fc1ce22d4bd5d8317676158419b48a9
radha-rani-n <nradharani1998@gmail.com>
2025-02-18 10:51:03 -0500

    add basic styling to header

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,10 +5,10 @@ const Navigation = () => {
     <>
       <nav className="nav-bar">
         <ul className="nav-bar__list">
-          <Link to="/your-trips">
+          <Link to="/your-trips" className="nav-bar__item-link">
             <li className="nav-bar__item">Your Trips</li>
           </Link>
-          <Link to="/">
+          <Link to="/" className="nav-bar__item-link">
             <li className="nav-bar__item">Home</li>
           </Link>
         </ul>
