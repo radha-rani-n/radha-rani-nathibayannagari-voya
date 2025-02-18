8da7d368bdfdfab9b50400ac1fd11a21533276ad
radha-rani-n <nradharani1998@gmail.com>
2025-02-18 11:03:41 -0500

    add hover and active styling to header

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,15 +1,34 @@
+import { useState, useEffect } from "react";
 import "./Navigation.scss";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 const Navigation = () => {
+  const location = useLocation();
+  const [yourTripsActive, setYourTripsActive] = useState(false);
+  useEffect(() => {
+    setYourTripsActive(location.pathname.includes("your-trips"));
+  }, [location.pathname]);
   return (
     <>
       <nav className="nav-bar">
         <ul className="nav-bar__list">
           <Link to="/your-trips" className="nav-bar__item-link">
-            <li className="nav-bar__item">Your Trips</li>
+            <li
+              className={`nav-bar__item ${
+                yourTripsActive ? "nav-bar__item--active" : ""
+              }`}
+            >
+              Your Trips
+            </li>
           </Link>
           <Link to="/" className="nav-bar__item-link">
-            <li className="nav-bar__item">Home</li>
+            <li
+              className={`nav-bar__item ${
+                !yourTripsActive ? "nav-bar__item--active" : ""
+              }`}
+            >
+              Home
+            </li>
           </Link>
         </ul>
       </nav>
