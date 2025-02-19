1db2bc9d6813658529592e5a7c24588d222a5971
radha-rani-n <nradharani1998@gmail.com>
2025-02-19 16:15:15 -0500

    add illustrations to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,28 @@
+import Travel from "../../assets/illustrations/Traveling-rafiki.svg";
+import travelGirl from "../../assets/illustrations/Traveling-bro.svg";
 import "./Footer.scss";
 const Footer = () => {
   return (
-    <section>
-      <h2>Voya</h2>
-      <div>
-        <p>About Creator</p>
-        <p>Home</p>
-        <p>Your Trips</p>
-      </div>
-    </section>
+    <footer className="footer">
+      <section className="footer__data">
+        <h2 className="footer__title">Voya</h2>
+        <ul className="footer__list">
+          <li className="footer__list-item">About Creator</li>
+          <li className="footer__list-item">Home</li>
+          <li className="footer__list-item">Your Trips</li>
+        </ul>
+        <img
+          src={Travel}
+          alt="Travel Illustration"
+          className="footer__illustration footer__illustration--1"
+        />
+        <img
+          src={travelGirl}
+          alt="Travel Illustration"
+          className="footer__illustration footer__illustration--2"
+        />
+      </section>
+    </footer>
   );
 };
 export default Footer;
