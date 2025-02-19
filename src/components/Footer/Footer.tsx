55f7cb4466505deaf5964f1d5af9a649d4e3b821
radha-rani-n <nradharani1998@gmail.com>
2025-02-19 17:10:24 -0500

    add chat form to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
 import Travel from "../../assets/illustrations/Traveling-rafiki.svg";
 import travelGirl from "../../assets/illustrations/Traveling-bro.svg";
+import { MessageCircle } from "lucide-react";
+import { Send } from "lucide-react";
 import "./Footer.scss";
+import { useState } from "react";
 const Footer = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <footer className="footer">
       <section className="footer__data">
@@ -21,6 +26,38 @@ const Footer = () => {
           alt="Travel Illustration"
           className="footer__illustration footer__illustration--2"
         />
+        <div className="footer__chat-widget">
+          <button
+            className="footer__chat-button"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {" "}
+            <MessageCircle size={24} />
+          </button>
+
+          {isOpen && (
+            <div className="footer__chat-window">
+              <div className="footer__chat-header">
+                <h3>Voya Support</h3>
+                <p>I'm Radha, creator of Voya:how can I help?</p>
+              </div>
+              <div className="footer__chat-form">
+                <textarea
+                  name=""
+                  id=""
+                  placeholder="Type your message..."
+                  className="footer__chat-input"
+                />
+                <button
+                  className="footer__chat-send"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  <Send size={20} className="footer__chat-send-icon" /> Send
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </section>
     </footer>
   );
