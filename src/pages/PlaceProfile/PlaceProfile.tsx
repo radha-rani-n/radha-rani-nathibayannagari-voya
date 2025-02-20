ff4d93c1c5d96e5b2ca23db9786943bd18117c61
radha-rani-n <nradharani1998@gmail.com>
2025-02-20 11:22:48 -0500

    position the plus on top right corner of image

--- src/pages/PlaceProfile/PlaceProfile.tsx
+++ src/pages/PlaceProfile/PlaceProfile.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import "./PlaceProfile.scss";
 import axios from "axios";
-import plus from "../../assets/icons/iconmonstr-heart-plus-lined.svg";
+
 import { DownOutlined } from "@ant-design/icons";
 import { useCallback, useEffect, useState } from "react";
 import type { MenuProps } from "antd";
@@ -141,20 +141,31 @@ const SearchBar = () => {
       <ul className="place-profile__places">
         {searchData.map((data, i: number) => (
           <li key={i} className="place-profile__place">
-            <div className="place-profile__img-name">
-              {data.photos && data.photos.length > 0 && (
-                <img
-                  className="place-profile__place-img"
-                  src={`https://places.googleapis.com/v1/${data.photos[0].name}/media?maxHeightPx=400&maxWidthPx=400&key=AIzaSyDD3fAb1QdZzEEn5ZJV7IlIQeUu9H8sdwU`}
-                />
-              )}
-              <p className="place-profile__place-name">
-                {data.displayName.text}
-              </p>
-            </div>
-            <div onClick={() => handlePlaceOnClick(data)}>
-              <img src={plus} alt="plus-icon" width="25" />
-            </div>
+            {data.photos && data.photos.length > 0 && (
+              <img
+                className="place-profile__place-img"
+                src={`https://places.googleapis.com/v1/${data.photos[0].name}/media?maxHeightPx=400&maxWidthPx=400&key=AIzaSyDD3fAb1QdZzEEn5ZJV7IlIQeUu9H8sdwU`}
+              />
+            )}
+            <p className="place-profile__place-name">{data.displayName.text}</p>
+
+            <svg
+              clipRule="evenodd"
+              fillRule="evenodd"
+              strokeLinejoin="round"
+              strokeMiterlimit="2"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              onClick={() => handlePlaceOnClick(data)}
+              className="place-profile__plus-icon"
+            >
+              <path
+                d="m14.121 19.337c-.467.453-.942.912-1.424 1.38-.194.189-.446.283-.697.283s-.503-.094-.697-.283c-4.958-4.813-9.303-8.815-9.303-12.54 0-3.348 2.582-5.177 5.234-5.177 1.831 0 3.636.867 4.766 2.563 1.125-1.688 2.935-2.554 4.771-2.554 2.649 0 5.229 1.815 5.229 5.168 0 .681-.144 1.37-.411 2.072-.375-.361-.798-.673-1.258-.925.113-.393.169-.773.169-1.147 0-2.52-1.933-3.668-3.729-3.668-1.969 0-3.204 1.355-4.159 2.694-.141.197-.369.314-.612.314-.243-.001-.471-.119-.611-.317-.953-1.347-2.165-2.699-4.155-2.7-.985 0-1.937.346-2.61.95-.735.658-1.124 1.602-1.124 2.727 0 2.738 3.046 5.842 8.5 11.127.346-.336.682-.663 1.007-.981.327.383.701.724 1.114 1.014zm3.38-9.335c2.58 0 4.499 2.107 4.499 4.499 0 2.58-2.105 4.499-4.499 4.499-2.586 0-4.5-2.112-4.5-4.499 0-2.406 1.934-4.499 4.5-4.499zm.5 3.999v-1.5c0-.265-.235-.5-.5-.5-.266 0-.5.235-.5.5v1.5h-1.5c-.266 0-.5.235-.5.5s.234.5.5.5h1.5v1.5c0 .265.234.5.5.5.265 0 .5-.235.5-.5v-1.5h1.499c.266 0 .5-.235.5-.5s-.234-.5-.5-.5z"
+                fill="white"
+                fillRule="nonzero"
+              />
+            </svg>
           </li>
         ))}
       </ul>
